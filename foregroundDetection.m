61a4b1d990f685e8858bb85585f43d93a074f7c2
TxT168 <1034971128@qq.com>
2018-03-17 18:53:53 +0800

    modify data path!

--- foregroundDetection.m
+++ foregroundDetection.m
@@ -2,12 +2,14 @@ clear,clc;
 
 total_num = 75;
 seg_rank = 30;
+path = '.\\CASIA_data\\DatasetA\\gaitdb\\fyc\\00_1\\fyc-00_1-0%02d.png';
+
 % 
 % bg = uint16(imread('.\CASIA_data\fyc\00_1\fyc-00_1-001.png'));
 % for i=2:total_num
-%     bg = bg + uint16(imread(sprintf('.\\CASIA_data\\fyc\\00_1\\fyc-00_1-0%02d.png',i)));
+%     bg = bg + uint16(imread(sprintf('.\\CASIA_data\\DatasetA\\gaitdb\\fyc\\00_1\\fyc-00_1-0%02d.png',i)));
 % end
-% vid = uint16(imread(sprintf('.\\CASIA_data\\fyc\\00_1\\fyc-00_1-0%02d.png',seg_rank)));
+% vid = uint16(imread(sprintf('.\\CASIA_data\\DatasetA\\gaitdb\\fyc\\00_1\\fyc-00_1-0%02d.png',seg_rank)));
 % 
 % bg = bg ./ total_num;
 % 
@@ -23,11 +25,11 @@ foregroundDetector = vision.ForegroundDetector('NumGaussians', 3, ...
 'NumTrainingFrames', 75);
 
 for i = 1:total_num
-    frame = imread(sprintf('.\\CASIA_data\\fyc\\00_1\\fyc-00_1-0%02d.png',i)); % read the next video frame
+    frame = imread(sprintf('.\\CASIA_data\\DatasetA\\gaitdb\\fyc\\00_1\\fyc-00_1-0%02d.png',i)); % read the next video frame
     step(foregroundDetector, frame);
 end
 
-frame = imread(sprintf('.\\CASIA_data\\fyc\\00_1\\fyc-00_1-0%02d.png',seg_rank)); % read the next video frame
+frame = imread(sprintf('.\\CASIA_data\\DatasetA\\gaitdb\\fyc\\00_1\\fyc-00_1-0%02d.png',seg_rank)); % read the next video frame
 foreground = step(foregroundDetector, frame);
 
 se = strel('square', 3);
