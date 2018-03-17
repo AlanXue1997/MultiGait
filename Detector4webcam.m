5a355de7bb9e6559dc9d0880308f85f8d7773349
AlanXue1997 <836106158@qq.com>
2018-03-17 20:45:07 +0800

    Add Detector4webcam

---
+++ Detector4webcam.m
@@ -0,0 +1,22 @@
+clear,clc;
+
+cam = webcam;
+cam.Resolution = '640x480';%'320x240';% '640x480';
+
+foregroundDetector = vision.ForegroundDetector('NumGaussians', 3, ...
+'NumTrainingFrames', 75);
+
+for i=1:100
+    img = snapshot(cam);
+    frame = step(foregroundDetector, img);
+    frame = uint8(frame);
+    subplot(1,2,1);
+    imshow(img);
+    img(:,:,1) = img(:,:,1).*frame;
+    img(:,:,2) = img(:,:,2).*frame;
+    img(:,:,3) = img(:,:,3).*frame;
+    subplot(1,2,2);
+    imshow(img);
+end
+
+clear('cam');
