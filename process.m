fd4be96ac2518d7b071435fc37c440bce05d5f82
TxT168 <1034971128@qq.com>
2018-03-22 22:01:49 +0800

    Merge branch 'Alanan_true' into LTC
    # Conflicts:
    #	.gitignore

--- process.m
+++ process.m
@@ -2,21 +2,106 @@ clear;clc;clf;
 
 image = imread('.\DGD_database\SAIVT-DGD-depthraw-0009\DGD\depth_raw\sub0001\nw01\0045.png');
 
-p=depthToCloud( image.*uint16(image<2047) );
+p=depth2cloud4dgd( image );
 
 x=p(:,:,1);
 y=p(:,:,2);
 z=p(:,:,3);
 
-x = x(:);
-y = y(:);
-z = z(:);
+a = [97,184];
+b = [101,284];
+r = b-a;
+q0 = ( (x-a(1))*r(2)-(z-a(2))*r(1) )<0;
 
+q = find((x>-70)+q0+(y>-15)+(y<60)+(z<600)+(z>0)==6);
+
+x = x(q);
+y = y(q);
+z = z(q);
+
+avgx = mean(x);
+avgy = mean(y);
+avgz = mean(z);
+
+x = (x - avgx);
+y = (y - avgy);
+z = (z - avgz);
+
+figure(3);
 scatter3(x,y,z,'.');
 
-axis equal
+height = 170;
+weight = 70;
 
-% a = [0.6,0.885];
+i = 1;
+j = 1;
+lenx = length(x);
+leny = length(y);
+
+while(i<=lenx)
+    if(abs(x(i))>height/2)
+        z(i)=[];
+        y(i)=[];
+        x(i)=[];
+        i = i-1;
+    end
+    i = i+1;
+    lenx = length(x);
+end
+
+while(j<=leny)
+    if(abs(z(j))>weight/2)
+        z(j)=[];
+        y(j)=[];
+        x(j)=[];
+        j = j-1;
+    end
+    j = j+1;
+    leny = length(y);
+end
+
+X = zeros(800,600);
+
+for i = 1 : length(x)
+    
+    if(int16((z(i)+0.5*weight)/weight*600)==0 && int16((x(i)+0.5*height)/height*800)==0)
+        
+        X( int16((x(i)+0.5*height)/height*800)+1,int16((z(i)+0.5*weight)/weight*600)+1 )=1;
+        
+    elseif (int16((x(i)+0.5*height)/height*800)==0)
+        
+        X( int16((x(i)+0.5*height)/height*800)+1,int16((z(i)+0.5*weight)/weight*600) )=1;
+        
+    elseif(int16((z(i)+0.5*weight)/weight*600)==0)
+        
+        X( int16((x(i)+0.5*height)/height*800),int16((z(i)+0.5*weight)/weight*600)+1 )=1;
+        
+    else
+        
+        X( int16((x(i)+0.5*height)/height*800),int16((z(i)+0.5*weight)/weight*600) )=1;
+        
+    end
+end
+
+figure(1);
+imshow(X);
+
+for i=1:size(X,1)
+    for j=1:size(X,2)
+        if X(i,j)==1
+            for k=j:600
+                % plot(i,k,'.');
+                X(i,k)=1;
+            end
+            break;
+        end
+    end
+end
+
+figure(2);
+imshow(X);
+
+% a = [0.6,0.885];888888888888888888888888888888888888888888888888888888888
 % b = [0.385,0.992];
 % %b = [0.6,0.89];
 % r = b-a;
